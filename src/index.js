d0d714e319fc4fcdffe2d17ab82cea83a8565988
Skylar Kesselring <skylarkesselring@gmail.com>
2022-09-24 00:25:40 -0400

    Add strikethrough functionality

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function renderTodos(todos) {
 
   todosContainer.innerHTML = todos.map(todo => (
     `<div class="todo">
-      <p class="todo__title" data-id=${todo.id}>${todo.title}</p>
+      <p class="todo__title todo__title--${todo.completed}" data-id=${todo.id}>${todo.title}</p>
       <button class="todo__edit-button" data-id=${todo.id}>edit</button>
       <button class="todo__delete-button" data-id=${todo.id}>delete</button>
     </div>`
@@ -48,11 +48,11 @@ async function initEvents() {
   todosContainer.addEventListener("click", e => {
     if (e.target.classList[0] === 'todo__edit-button') editTodo(e);
     if (e.target.classList[0] === 'todo__delete-button') deleteTodo(e.target.dataset.id);
-    if (e.target.classList[0] === 'todo__title') console.log(e.target.dataset.id);
+    if (e.target.classList[0] === 'todo__title') completedTodo(e);
   })
 }
 
-// ADD
+// ADD TODO
 async function addTodo() {
 
   const newTodo = {
@@ -67,11 +67,11 @@ async function addTodo() {
     headers: {'Content-Type': 'application/json'}
   });
   const data = await response.json(); 
-  state.setTodos = [...state.getTodos, data];
+  state.setTodos = [data, ...state.getTodos];
   renderTodos(state.getTodos);
 }
 
-// DELETE
+// DELETE TODO
 async function deleteTodo(id) {
 
   await fetch(API_URL + id, {
@@ -82,15 +82,15 @@ async function deleteTodo(id) {
   renderTodos(state.getTodos);
 }
 
-// EDIT
-async function editTodo(e) {
+// EDIT TODO
+function editTodo(e) {
 
   let todo = state.getTodos.find(todo => todo.id === e.target.dataset.id);
 
   e.target.parentElement.innerHTML = `
     <input class="todo__edit-input" value="${todo.title}"}/>
     <button class="todo__edit-button--submit" data-id=${todo.id}>submit changes</button>
-    <button class="todo__delete-button" data-id=${todo.id}>delete</button>
+    <button class="todo__delete-button" data-id=${todo.id} type="submit">delete</button>
   `;
 
   const editInputField = document.querySelector('.todo__edit-input');
@@ -102,8 +102,76 @@ async function editTodo(e) {
       <button class="todo__edit-button" data-id=${todo.id}>edit</button>
       <button class="todo__delete-button" data-id=${todo.id}>delete</button>
     `;
+
+    fetch(API_URL + todo.id, {
+      method: 'PATCH',  
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        title: editInputField.value
+      })
+    })
+    .then(res => res.json())
+    .then(data => state.setTodos = [...state.getTodos.filter(ele => ele.id !== todo.id), data]);
   });
 }
+
+// COMPLETE TODO
+async function completedTodo(e) {
+
+  let currentTodo = state.getTodos.find(todo => todo.id === e.target.dataset.id);
+
+  const response = await fetch(API_URL + currentTodo.id, {
+    method: 'PATCH',  
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      completed: !currentTodo.completed
+    })
+  });
+  const data = await response.json();
+
+  if (data.completed) state.setTodos = [...state.getTodos.filter(todo => todo.id !== e.target.dataset.id), data];
+  else state.setTodos = [data, ...state.getTodos.filter(todo => todo.id !== e.target.dataset.id)];
+  
+  renderTodos(state.getTodos);
+}
+
+
+
+
+
+
+
+
+
+// second way to submit edit. allows enter functionality, but has a warning message about form submission canceling.
+
+// async function editTodo(e) {
+
+//   let todo = state.getTodos.find(todo => todo.id === e.target.dataset.id);
+
+//   e.target.parentElement.innerHTML = `
+//     <form class="todo__form">
+//       <input class="todo__edit-input" value="${todo.title}"}/>
+//       <button class="todo__edit-button--submit" data-id=${todo.id}>submit changes</button>
+//       <button class="todo__delete-button" data-id=${todo.id} type="submit">delete</button>
+//     </form>
+//   `;
+
+//   const editInputField = document.querySelector('.todo__edit-input');
+//   const todoForm = document.querySelector('.todo__form');
+
+//   todoForm.addEventListener("submit", event => {
+//     e.preventDefault();
+//     event.target.parentElement.innerHTML = `
+//       <form class="todo__form"></form>
+//       <p class="todo__title" data-id=${todo.id}>${editInputField.value}</p>
+//       <button class="todo__edit-button" data-id=${todo.id}>edit</button>
+//       <button class="todo__delete-button" data-id=${todo.id}>delete</button>
+//     `;
+//   });
+// }
+
+
 
 
 initEvents();
